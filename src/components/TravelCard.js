dce572b25f4ce5a52eb5ed734a60fb2f11f23fd6
Ulla <ullavansprang@gmail.com>
2022-10-03 18:07:35 +0200

    Destructured props

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-export default function TravelCard(props) {
+export default function TravelCard({
+  img,
+  alt,
+  country,
+  link,
+  title,
+  date,
+  description,
+}) {
   return (
     <div className="travel-card">
       <div className="travel-card-image">
-        <img
-          src={`../images/${props.img}`}
-          alt={props.alt}
-          width="125"
-          height="168"
-        />
+        <img src={`../images/${img}`} alt={alt} width="125" height="168" />
       </div>
       <div className="travel-card-details">
         <img
@@ -19,13 +22,13 @@ export default function TravelCard(props) {
           width="7"
           className="travel-card-icon"
         />
-        <h3 className="travel-card-country">{props.country}</h3>
-        <a href={props.link} className="travel-card-link">
+        <h3 className="travel-card-country">{country}</h3>
+        <a href={link} className="travel-card-link">
           View on Google Maps
         </a>
-        <h2 className="travel-card-title">{props.title}</h2>
-        <p className="travel-card-dates">{props.date}</p>
-        <p className="travel-card-description">{props.description}</p>
+        <h2 className="travel-card-title">{title}</h2>
+        <p className="travel-card-dates">{date}</p>
+        <p className="travel-card-description">{description}</p>
       </div>
     </div>
   );
